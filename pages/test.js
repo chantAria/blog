7516c0f329dd2744ff2d4bc091ee79cae1d2f348
chantAria <zxy3560@qq.com>
2021-06-25 03:16:01 +0800

    day3.71

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,6 @@
 import Parallax from 'parallax-js'
 import { useEffect, useState, useRef, useLayoutEffect } from 'react'
-import React, { Component } from 'react'
+
 //防抖函数
 const debounce = (fn, delay) => {
   let timer = null
@@ -12,88 +12,64 @@ const debounce = (fn, delay) => {
   }
 }
 
-export default class Screen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      style: {},
-      size: {},
-    }
-    this.ref = React.createRef()
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize)
-    const el = document.getElementById('parallax-box')
-    new Parallax(el)
+export default function Screen() {
+  const [style, setStyle] = useState({}) //定义需要应用的style
+  const ref = useRef() //获取img来得到宽高
+  const onResize = () => {
     const size = {
       width: document.documentElement.clientWidth,
       height: document.documentElement.clientHeight,
     }
-    const width = this.ref.current.width
-    const height = this.ref.current.height
+    const width = ref.current.width
+    const height = ref.current.height
     let z
-    width / size.width > height / size.height
-      ? (z = height / size.height)
-      : (z = width / size.width)
-    console.log(width, height, z)
-    const style = {
+    if (width / size.width > height / size.height) {
+      //竖屏
+      z = height / size.height
+    } else {
+      //横屏
+      z = width / size.width
+    }
+    setStyle({
       width: (width / z) * 1.2,
       height: (height / z) * 1.2,
-      marginLeft: -0.1 * width,
-      marginTop: -0.1 * height,
+      marginLeft: -0.08 * (width / z),
+      marginTop: -0.08 * (height / z),
+    })
+  }
+  const deb = debounce(onResize, 10)
+  //刚加载时Parallax化
+  useEffect(() => {
+    const el = document.getElementById('parallax-box')
+    new Parallax(el)
+  }, [])
+  //窗口尺寸变化监听
+  useLayoutEffect(() => {
+    window.addEventListener('resize', deb)
+    onResize()
+    return () => {
+      window.removeEventListener('resize', deb)
     }
-    this.setState({
-      size,
-      style,
-    })
-  }
+  }, [])
 
-  componentWillUpdate() {
-    window.removeEventListener('resize', this.onResize)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize)
-  }
-
-  onResize = () => {
-    this.setState({
-      size: {
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight,
-      },
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <div id='parallax-box' data-clip-relative-input>
-          <img
-            ref={this.ref}
-            data-depth='0.6'
-            src='screen.jpg'
-            style={this.state.style}
-          ></img>
-        </div>
-        <div className='content'>111</div>
-        <style jsx>{`
-          #parallax-box {
-            height: 100vh;
-            width: 100vw;
-            overflow: hidden;
-            position: relative;
-          }
-          img {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-          }
-          .content {
-            height: 300px;
-          }
-        `}</style>
+  return (
+    <div>
+      <div id='parallax-box' data-clip-relative-input>
+        <img ref={ref} data-depth='0.6' src='screen.jpg' style={style}></img>
       </div>
-    )
-  }
+      <div className='content'>111</div>
+      <style jsx>{`
+        #parallax-box {
+          height: 100vh;
+          width: 100%;
+          overflow: hidden;
+          position: relative;
+          text-align: center;
+        }
+        .content {
+          height: 300px;
+        }
+      `}</style>
+    </div>
+  )
 }
